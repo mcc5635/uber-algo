bab3189328eed19b7f21c278ac0a73889cae94eb
Michael Chase <themikechase@gmail.com>
2024-06-27 07:36:56 -0700

    adding final solution

--- engine.py
+++ engine.py
@@ -150,21 +150,103 @@ profits = distances.apply(lambda x: x + rng.lognormal(mean=1, sigma=3))
 # print(profits)
 
 
+num_drivers = len(eta1.values[0,:]) # column length
+num_riders = len(eta1.values[:,0]) #row length
+
+drivers = cp.Variable(profits.shape, integer=True)
+
+total_profit = sum([profits.values[i,j]*drivers[i,j] for i in range(profits.shape[0])
+  for j in range(profits.shape[1])])
+
+objective_function = cp.Maximize(total_profit)
+
+cons1 = []
+
+#Scenario 1
+if num_riders == num_drivers:
+
+  for i in range(len(profits.index)):
+    cons1.append(sum(drivers[i,j] for j in range(maximum_dispatch_radius.shape[1]))==1)
+
+  for j in range(len(profits.columns)):
+    cons1.append(sum(drivers[i,j] for i in range(maximum_dispatch_radius.shape[0]))==1)
+
+    cons1.append(sum(maximum_dispatch_radius.values[i,j]*drivers[i,j] for i in range(maximum_dispatch_radius.shape[0]) 
+  for j in range(maximum_dispatch_radius.shape[1])) == len(profits.columns))
+
+#Scenario 2
+elif num_riders > num_drivers:
+
+  for i in range(len(profits.index)):
+    cons1.append(sum(drivers[i,j] for j in range(maximum_dispatch_radius.shape[1]))<=1)
+
+  for j in range(len(profits.columns)):
+    cons1.append(sum(drivers[i,j] for i in range(maximum_dispatch_radius.shape[0]))==1)
+  
+  cons1.append(sum(maximum_dispatch_radius.values[i,j]*drivers[i,j] for i in range(maximum_dispatch_radius.shape[0]) 
+  for j in range(maximum_dispatch_radius.shape[1])) == len(profits.columns))
+
+#Scenario 3
+elif num_riders < num_drivers:
+
+  for i in range(len(profits.index)):
+    cons1.append(sum(drivers[i,j] for j in range(maximum_dispatch_radius.shape[1]))==1)
+    
+  for j in range(len(profits.columns)):
+    cons1.append(sum(drivers[i,j] for i in range(maximum_dispatch_radius.shape[0]))<=1)
+    
+  cons1.append(sum([maximum_dispatch_radius.values[i,j]*drivers[i,j] for i in range(maximum_dispatch_radius.shape[0])
+    for j in range(maximum_dispatch_radius.shape[1])]) == len(profits.index))
+
+###############################################################
+## Section 4:  Optimal Scheduling of Uber Push Notifications ##
+###############################################################
+
+rng = np.random.default_rng(42)
+# Number of times that you can schedule a push notification
+time_periods = rng.integers(1,40) # 4
+
+# Number of push notifications you need to schedule
+num_notifications = rng.integers(1,40) # 9
+
+# Goal is to schedule NOTIFICATION to TIME PERIOD
+
+# Score that measures the value of a (push, time) pair
+# It represents the output of a machine learning model 
+# that predicts the probability of a user making an order within 24 hours of receiving push i at time t
+scores = pd.DataFrame(rng.uniform(0, 50, (num_notifications, time_periods)),
+                         columns=[f'Time Slot {j}' for j in range(1, time_periods+1)],
+                         index=[f'Push Notification {i}' for i in range(1,num_notifications+1)])
+# print(scores)
+
+send = cp.Variable(scores.shape, integer=True)
+
+notification_profit = sum([scores.values[i,j]*send[i,j] for i in range(scores.shape[0])
+  for j in range(scores.shape[1])])
+
+objective_function = cp.Maximize(notification_profit)
+
+# Constraints
+pushy = []
+
+x = [send >= 0] #controll that send variable is binary between 0 and 1
+
+y = [send <= 1] #controll that only one notification is sent
 
 
+#controlling rows: constraining to send at most one of each push notification
+for i in range(len(scores.index)):
+    pushy.append(sum([send[i,j] for j in range(scores.shape[1])])<= 1)
+
+#controlling columns: #Send at most F(2) pushes in the time horizon
+for j in range(len(scores.columns)):
+    pushy.append(sum(send[i,j] for i in range(scores.shape[0])) <=2)
 
 
-
-
-
-
-
-
-
-
-
-
-
+constraint = pushy*x+y
+prob = cp.Problem(objective_function, constraint)
+prob.solve(), send.value
+# print(prob)
 
 
 
